d3ad106617e522d16a2f11d6c22e0f4b87dec8a3
ShiminTang <tangshimin2017@gmail.com>
2025-01-14 18:07:43 -0500

    update for multi-rec

--- scripts/hyperct_ai/run/run_ai_loop.sh
+++ scripts/hyperct_ai/run/run_ai_loop.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 source /home/gxt/miniconda3/bin/activate /home/gxt/miniconda3/envs/image
 
-rec_jobID=$(sbatch --parsable /home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/rec_1.sh)
-echo "recon job id is $rec_jobID"
+search_dir=/home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run
 
-eva_jobID=$(sbatch --parsable --dependency=afterok:${rec_jobID} /home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/eva.sh)
+jobIDs=()
+for recfile in "$search_dir"/rec*
+do
+    rec_jobID=$(sbatch --parsable $recfile)
+    echo "recon job id is $rec_jobID"
+    jobIDs+=("$rec_jobID")
+done
+
+eva_jobID=$(sbatch --parsable --dependency=afterok:$(echo ${jobIDs[*]} | tr ' ' ,) /home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/eva.sh)
 echo "Evaluate job id is $eva_jobID"
 
 sbatch --dependency=afterok:${eva_jobID} /home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/ang_prop.sh
