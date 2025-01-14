014c48551b36a33f7c32e67b27a88f0e3319c96f
ShiminTang <tangshimin2017@gmail.com>
2025-01-14 18:07:06 -0500

    update with args mode and generate multiple rec.sh for multi-bands

--- scripts/hyperct_ai/run/create_shell.sh
+++ scripts/hyperct_ai/run/create_shell.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-output_file=test_leile # change for output file name
-wav_id=0
+echo ">>Remove old script first"
+rm rec_*.sh eva.sh ang_prop.sh
+
+log_pth=$1 #
+file_prefix=$2 # change for output file name
+#wav_id=0
 
 TMP_PTH="/home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/templates"
 DES_PTH="/home/gxt/Projects/hype_ipts_33531/scripts/hyperct_ai/run/"
 
 tmp_pth="$TMP_PTH/rec_tmp.sh"
-file_pth="$DES_PTH/rec_1.sh"
 
-sed "s/OUTPUT_FILE/$output_file/g; s/WAVID/$wav_id/g" "$tmp_pth" > "$file_pth"
+for i in ${@:4}
+do
+    file_pth="$DES_PTH/rec_$i.sh"
+    sed "s|LOG_PTH|$log_pth|g; s/PREFIX/$file_prefix/g; s/WAVID/$(($i-1))/g" "$tmp_pth" > "$file_pth"
+done
 
 tmp_pth="$TMP_PTH/eva_tmp.sh"
 file_pth="$DES_PTH/eva.sh"
 
-sed "s/OUTPUT_FILE/$output_file/g" "$tmp_pth" > "$file_pth"
+sed "s|LOG_PTH|$log_pth|g; s/PREFIX/$file_prefix/g" "$tmp_pth" > "$file_pth"
 
 tmp_pth="$TMP_PTH/ang_prop_tmp.sh"
 file_pth="$DES_PTH/ang_prop.sh"
-angle_mode=preset
+angle_mode=$3 #preset
 
-sed "s/OUTPUT_FILE/$output_file/g; s/ANGLE_MODE/$angle_mode/g" "$tmp_pth" > "$file_pth"
+sed "s|LOG_PTH|$log_pth|g; s/PREFIX/$file_prefix/g; s/ANGLE_MODE/$angle_mode/g" "$tmp_pth" > "$file_pth"
