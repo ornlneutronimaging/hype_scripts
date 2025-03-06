7604c4d2562e3beff49e0c2fca7b98de92a2ae49
JeanBilheux <bilheuxjm@ornl.gov>
2025-03-06 12:50:46 -0500

    changing code to make it work with timepix3

--- scripts/cron_job_script_pre_processing.sh
+++ scripts/cron_job_script_pre_processing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Last time running cron_job_script_pre_processing.sh: " >> /SNS/VENUS/IPTS-33531/shared/ai_output/logs/cron_jobs.txt
-date >> /SNS/VENUS/IPTS-33531/shared/ai_output/logs/cron_jobs.txt
+echo "Last time running cron_job_script_pre_processing.sh: " >> /SNS/VENUS/IPTS-35790/shared/hype/logs/cron_jobs.txt
+date >> /SNS/VENUS/IPTS-35790/shared/hype/logs/cron_jobs.txt
 source /opt/anaconda/etc/profile.d/conda.sh
 conda activate /home/j35/.conda/envs/ImagingReduction
-python /SNS/VENUS/IPTS-33531/shared/hype_ipts_33531/scripts/run_autoreduction_and_move_folders.py
+python /SNS/VENUS/IPTS-33531/shared/hype_ipts_33531/scripts/move_folders.py
 
 ## this cron job will be ran from hype every minute !
